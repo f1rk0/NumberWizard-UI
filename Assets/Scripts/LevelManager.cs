118d8926255194980780c3458242b4357f36adf0
f1rk0 <f1rk0top1@gmail.com>
2017-01-11 13:59:17 +0300

    V1.0. Number Guessing game. You pick, computer guess. Simple as that.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log("Level going" + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void Quit(){
+		Debug.Log("Level quit");
+		Application.Quit();
+	}
+
+}
---
+++ Assets/Scripts/NumberWizardUI.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizardUI : MonoBehaviour {
+	
+	// Use this for initialization	
+	
+	int max;
+	int min;
+	int guess;
+	int mid;
+	int range;
+	
+	public int maxguess = 8;
+	
+	public Text text;
+	
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame (){		
+		max = 1000;
+		min = 1;	
+		NextGuess();
+	}
+	
+	void NextGuess (){
+		mid = (min+max)/2;		
+		range = (max-min)/2;		
+		guess = mid + Random.Range(0,range+1) - Random.Range(0,range+1);	
+		text.text = guess.ToString();
+		maxguess = maxguess - 1;
+		if (maxguess <= 0)			{Application.LoadLevel("Win");}
+	}
+	
+	public void GuessHigh(){
+		min = guess;
+		NextGuess();
+	}
+	
+	public void GuessLower(){
+		max = guess;
+		NextGuess();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
